315f38f60df82f88f124101da02d772f4b7ea476
Skanda lin <anikiwayne@gmail.com>
2020-02-13 11:40:15 +0800

    updated remove token

--- client/src/components/nav/sideNav.vue
+++ client/src/components/nav/sideNav.vue
@@ -37,7 +37,7 @@ export default {
 
             let i = this.$store.dispatch('postApi', data);
             i.then((result)=>{
-                // localStorage.removeItem('didi_token')
+                localStorage.removeItem('didi_token')
                 console.log(result)
                 if(result['data'].success) {
                     let msg = {}
